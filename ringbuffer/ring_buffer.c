530cbf1bf1e2337ef62fad4f7c34693fc5be5937
qigaoqiang <qgq3562@163.com>
2022-04-24 00:18:57 +0800

    Add ringbuffer.c to be implemented

---
+++ ringbuffer/ring_buffer.c
@@ -0,0 +1,17 @@
+// https://blog.csdn.net/qq_33907392/article/details/100651928
+
+#define N 1024
+char buffer[N];
+int rp = 0, wp = 0;
+
+int write(char *ptr, int len)
+{
+    
+}
+
+
+int read(char *ptr, int len)
+{
+
+
+}
